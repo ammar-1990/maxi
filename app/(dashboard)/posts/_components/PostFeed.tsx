a33a7f99a3569a2b5bf8f9a3f48b4d1fa349d320
ammar <ammar.ali.haidar.1990@gmail.com>
2025-03-04 19:51:38 +0400

    fix(filter post):'made'

--- app/(dashboard)/posts/_components/PostFeed.tsx
+++ app/(dashboard)/posts/_components/PostFeed.tsx
@@ -3,11 +3,17 @@ import Link from 'next/link'
 import React from 'react'
 import PostCard from './PostCard'
 
-type Props = {}
+type Props = {categorySlug:string | undefined,subCategorySlug:string | undefined,postType:string | undefined}
 
-const PostFeed =async (props: Props) => {
+const PostFeed =async ({categorySlug,postType,subCategorySlug}: Props) => {
 
-    const posts = await prisma.post.findMany()
+    const posts = await prisma.post.findMany({
+      where:{
+        ...(categorySlug && {subCategory:{category:{slug:categorySlug}}}),
+        ...(postType && {postType:{slug:postType}}),
+        ...(subCategorySlug && {subCategory:{ slug:subCategorySlug}}),
+      }
+    })
   return (
     <div className='grid grid-cols-1 md:grid-cols-3'>
         {posts.map(item=><PostCard key={item.id} post={item} />)}
--- app/(dashboard)/posts/page.tsx
+++ app/(dashboard)/posts/page.tsx
@@ -4,10 +4,34 @@ import SuperButton from '@/components/SuperButton'
 import { PlusCircle } from 'lucide-react'
 import SuspenseComponent from '@/components/SuspenseComponent'
 import PostFeed from './_components/PostFeed'
+import prisma from '@/lib/prisma'
+import Search from '@/components/Search'
 
-type Props = {}
+type Props = {
+  searchParams:Promise<{categorySlug:string | undefined,subCategorySlug:string | undefined,postType:string | undefined}>
+}
 
-const PostsPage = (props: Props) => {
+const PostsPage =async ({searchParams}: Props) => {
+   const {categorySlug,postType,subCategorySlug} = await searchParams
+const categoriesRes = prisma.category.findMany({
+  select:{
+    slug:true,name:true
+  }
+})
+const subCategoryRes = prisma.subCategory.findMany({
+  select:{
+    slug:true,name:true
+  }
+})
+const postTypeRes = prisma.postType.findMany({
+  select:{
+    slug:true,name:true
+  }
+})
+
+
+const [categories, subCategories, postTypes] = await Promise.all([categoriesRes,subCategoryRes,postTypeRes])
+
   return (
     <div>
       <div className='flex items-center justify-between'>
@@ -15,9 +39,28 @@ const PostsPage = (props: Props) => {
      <SuperButton buttonType='linkButton' title='Create Post' Icon={<PlusCircle />} href='/posts/new' />
   
       </div>
+
+      <div className='mt-12 flex items-center gap-5'>
+    <Search inputType='select' label='Categories' searchParam='categorySlug' values={categories} renderItem={async(item)=>{
+      'use server'
+      return { label: item.name, value: item.slug };
+
+    }} />
+    <Search inputType='select' label='Sub-Category' searchParam='subCategorySlug' values={subCategories} renderItem={async(item)=>{
+      'use server'
+      return { label: item.name, value: item.slug };
+
+    }} />
+    <Search inputType='select' label='Post Type' searchParam='postType' values={postTypes} renderItem={async(item)=>{
+      'use server'
+      return { label: item.name, value: item.slug };
+
+    }} />
+      </div>
+
       <div className='mt-12'>
         <SuspenseComponent>
-          <PostFeed/>
+          <PostFeed categorySlug={categorySlug} postType={postType} subCategorySlug={subCategorySlug}/>
         </SuspenseComponent>
 
       </div>
